d82f55ced32a9cd85bca8df4b8ca45111acdba05
G4bbs <gabriel03082005@edu.unipar.br>
2024-10-22 22:07:49 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <Header
+        texto="Iphone 15 PRO: R$ 4.780"
+        image="https://i.zst.com.br/thumbs/12/25/34/-1114443995.jpg"
+      />
+      <h1></h1>
+
+      <Header
+        texto="Iphone 14: R$ 4.190"
+        image="https://i.zst.com.br/thumbs/12/f/14/-871950416.jpg"
+      />
+      <Header
+        texto="Iphone 13: R$ 3.689"
+        image="https://i.zst.com.br/thumbs/12/17/34/-747048218.jpg"
+      />
+    </>
+  );
+}
+
+export default App;
